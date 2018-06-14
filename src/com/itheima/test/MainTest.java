ac8b6fb138ec76e37b86298335a771e0844e0f2e
jm2981858 <516359860@qq.com>
2018-06-14 23:12:26 +0800

    06

--- src/com/itheima/test/MainTest.java
+++ src/com/itheima/test/MainTest.java
@@ -14,7 +14,18 @@ public class MainTest {
 		Statement st = null;
 		ResultSet rs = null;
 		try {
-			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			conn = JDBCUtil.getConn();
+			st = conn.createStatement();
+			String sql = "select * from t_stu";
+			ts = st.executeQuery(sql);
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				
+				System.out.println("id="+id+"===name="+name+);
+			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
