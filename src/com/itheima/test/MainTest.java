6d7b3aee32d2074cb82222cdc232534ff4647163
jm2981858 <516359860@qq.com>
2018-06-14 21:58:27 +0800

    03

--- src/com/itheima/test/MainTest.java
+++ src/com/itheima/test/MainTest.java
@@ -1,18 +1,24 @@
 package com.itheima.test;
 
+import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainTest {
 
 	public static void main(String[] args) {
-		
+		Connection conn  = null;
+		Statement st = null;
+		ResultSet rs = null;
 		try {
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 }
